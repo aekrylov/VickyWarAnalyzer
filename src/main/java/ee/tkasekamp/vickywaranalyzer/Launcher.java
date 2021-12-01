8d2013c5402fa25a34bed53dfa1a0f822cd2035a
Anton Krylov <anton.krylov@piano.io>
2021-12-01 13:29:03 +0300

    make it work when JavaFX not installed on the host system

---
+++ src/main/java/ee/tkasekamp/vickywaranalyzer/Launcher.java
@@ -0,0 +1,11 @@
+package ee.tkasekamp.vickywaranalyzer;
+
+/**
+ * This is needed for javafx to work properly when not installed on the OS level
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
